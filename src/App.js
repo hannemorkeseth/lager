f5a425b81f006099d9787b5902c16e328d3657ec
Hanne Mørkeseth <hanne@morkeseth.no>
2021-11-09 19:18:06 +0100

    added list-component

--- src/App.js
+++ src/App.js
@@ -1,117 +1,20 @@
 import React from 'react'
-import ListItem from './ListItem'
-import InputField from './InputField'
-import data from './data'
+import lister from './lister'
+import List from './List'
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      data: data,
-      inputvalue: ''
+      lister
     }
-    this.saveStateToLocalStorage = this.saveStateToLocalStorage.bind(this)
-    this.getStateFromLocalStorage = this.getStateFromLocalStorage.bind(this)
-    this.handlePlusClick = this.handlePlusClick.bind(this)
-    this.handleMinusClick = this.handleMinusClick.bind(this)
-    this.handleDelete = this.handleDelete.bind(this)
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.addItem = this.addItem.bind(this)
   }
-
-  saveStateToLocalStorage() {
-    localStorage.setItem('state', JSON.stringify(this.state.data))
-  }
-
-  getStateFromLocalStorage() {
-    this.setState(() => {
-      const data = localStorage.getItem('state')
-      console.log(data);
-      return {
-        data: JSON.parse(data)
-      }
-
-    })
-  }
-
-  componentDidMount() {
-    this.getStateFromLocalStorage()
-  }
-
-  handlePlusClick(id) {
-    this.setState(prevState => {
-    const newData = prevState.data.map(vare => {
-      if (vare.id === id) {
-        return {
-          ...vare,
-          antall: vare.antall + 1
-        }
-      }
-        return vare
-      })
-      return {
-        data: newData
-      }
-     })
-  }
-
-  handleMinusClick(id) {
-    this.setState(prevState => {
-    const newData = prevState.data.map(vare => {
-      if (vare.id === id) {
-        return {
-          ...vare,
-          antall: vare.antall - 1
-        }
-      }
-        return vare
-      })
-      return {
-        data: newData
-      }
-     })
-  }
-
-  handleDelete(id) {
-    this.setState(prevState => {
-      const filteredData = prevState.data.filter(vare => {
-      if (vare.id !== id) {
-        return vare
-      }
-      })
-      return {
-        data: filteredData
-      }
-    })
-  }
-
-  handleInputChange(value) {
-    this.setState({inputvalue: value})
-  }
-
-  addItem(item) {
-    this.setState(prevState => ({
-      data: [...prevState.data,
-            {vare: item, antall: 1, id: item}]
-      })
-    )
-    this.setState({inputvalue: ''})
-  }
-
 
   render() {
-    const varer = this.state.data.map((vare, index) => <ListItem
-      key={`${index}`}
-      vare={vare}
-      handlePlusClick={this.handlePlusClick}
-      handleMinusClick={this.handleMinusClick}
-      handleDelete={this.handleDelete} />)
+    const varelister = this.state.lister.map(liste => <List key={liste.id} tittel={liste.tittel} saveState={liste.state} />)
     return (
-      <div className="app">
-        <h1 className="overskrift">Matvarelager Ullerøy</h1>
-        {varer}
-        <InputField value={this.state.inputvalue} handleInputChange={this.handleInputChange} addItem={this.addItem}/>
-        <button onClick={this.saveStateToLocalStorage} className="lagre">Lagre</button>
+      <div>
+        {varelister}
       </div>
     )
   }
---
+++ src/lister.js
@@ -0,0 +1,14 @@
+const lister = [
+  {
+    id: 1,
+    tittel: "Liste 1",
+    state: "state1"
+  },
+  {
+    id: 2,
+    tittel: "Liste 2",
+    state: "state2"
+  }
+]
+
+export default lister
