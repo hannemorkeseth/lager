661f3c2ebb25c50e906abece5e3e53637fea1f6d
Hanne Mørkeseth <hanne@morkeseth.no>
2021-11-10 19:20:30 +0100

    styling fra state

--- src/ListItem.js
+++ src/ListItem.js
@@ -4,10 +4,10 @@ function ListItem(props) {
   return (
     <div className="list-item-container">
       <p className="text-field">{props.vare.vare}</p>
-      <button className="btn" onClick={() => {props.handlePlusClick(props.vare.id)}}>+</button>
+      <button className="btn" onClick={() => {props.handlePlusClick(props.vare.id)}} style={{backgroundColor: props.buttonColor}}>+</button>
       <p className="number">{props.vare.antall}</p>
-      <button className="btn" onClick ={() => {props.handleMinusClick(props.vare.id)}}>-</button>
-      <button className="delete-btn" onClick = {() => {props.handleDelete(props.vare.id)}} >Delete</button>
+      <button className="btn" onClick ={() => {props.handleMinusClick(props.vare.id)}} style={{backgroundColor: props.buttonColor}}>-</button>
+      <button className="delete-btn" onClick = {() => {props.handleDelete(props.vare.id)}} >Slett</button>
     </div>
   )
 }
